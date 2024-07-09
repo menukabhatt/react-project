b8636009b819fa9ea054eb9be12ee869b33122a9
menukabhatt <menukabhatt73@gmail.com>
2024-07-09 19:15:49 +0545

    push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home';
-import About from './Pages/About';
+// import About from './Pages/About';
 import Contact from './Pages/Contact';
-import Page1 from './Pages/Subpages/Page1';
-import Page2 from './Pages/Subpages/Page2';
+// import Page1 from './Pages/Subpages/Page1';
+// import Page2 from './Pages/Subpages/Page2';
 import RootLayout from './Components/RootLayout';
 import DetailPage from './Pages/Subpages/Detailpage';
 
@@ -12,20 +12,20 @@ const App = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <RootLayout/>,
-      children:[
+      element: <RootLayout />,
+      children: [
         {
-          index:true,
-          element: <Home/>
+          index: true,
+          element: <Home />
         },
         {
-          path:'detail/:id',
-          element: <DetailPage/>
+          path: 'detail/:id',
+          element: <DetailPage />
         },
-        {
-          path: 'about-page',
-          element: <About />
-        },
+        // {
+        //   path: 'about-page',
+        //   element: <About />
+        // },
         {
           path: 'contact-page',
           element: <Contact />
@@ -33,7 +33,7 @@ const App = () => {
 
       ]
     },
-   
+
   ]);
 
   return <RouterProvider router={router} />
